0814db38f67bf50bffc19474a06eee1d1f158e42
FAN CHANG <fanchang@gmail.com>
2024-04-16 11:21:15 -0700

    files from mac

---
+++ ch1/short20.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char ex[20];
+    puts("Entrer boyfriend's name: ");
+    scanf("%19s", ex);
+    printf("Dear %s.\n\n\tYou're history.\n", ex);
+    return 0;
+}
