f2777c578e0e31fc9bc4911ac6a60679d22babc2
FAN CHANG <fanchang@gmail.com>
2024-08-01 21:19:33 -0700

    if else statement 	../240729/main

---
+++ 240801/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+	int number;
+	printf("Input a number ");
+	scanf("%d", &number);
+	if (number == 0)
+	{
+		printf("The number is zero.\n");
+	}
+	else if (number >0)
+	{
+		printf("The number is positive.\n");
+	}
+	else
+	{
+		printf("The number is negative.\n");
+	}
+	return 0;
+}
