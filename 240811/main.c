81c2b868af32ed4ea28fa7555139c54792d00203
FAN CHANG <fanchang@gmail.com>
2024-08-11 12:09:53 -0700

    multi dimension array in c program 	new file:   main.c

---
+++ 240811/main.c
@@ -0,0 +1,13 @@
+// multi-dimension array in c program
+#include <stdio.h>
+
+int main() {
+	int arr[2][3] = { {1, 3, 5}, {2, 4, 6}};
+	for (int i=0; i<2; i++){
+		for (int j=0; j<3; j++) {
+			printf("%d\n", arr[i][j]);
+		}
+	}
+	
+	return 0;
+}
